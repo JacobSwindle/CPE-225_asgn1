651a786b829acf24814745d4d038c7235aae6a65
Jacob Swindle <jaswindl@calpoly.edu>
2022-03-31 12:19:12 -0700

    1

--- binary.py
+++ binary.py
@@ -12,7 +12,37 @@ def add(addend_a, addend_b):
     :param addend_b: A bitstring representing the second number
     :return: A bitstring representing the sum
     """
-    pass
+    result = ''
+    carry = 0
+    for i in range(15, -1, -1):
+        bit_a = addend_a[i]
+        bit_b = addend_b[i]
+        one_count = 0
+        next = -1
+
+        if (bit_a == '1'):
+            one_count += 1
+        if (bit_b == '1'):
+            one_count += 1
+        if (carry == 1):
+            one_count += 1
+        
+        if (one_count == 0):
+            carry = 0 ##
+            next = '0'
+        elif (one_count == 1):
+            carry = 0
+            next = '1'
+        elif (one_count == 2):
+            carry = 1
+            next = '0'
+        else:
+            carry = 1
+            next = '1'
+        
+        result = next + result
+    
+    return result
 
 
 def negate(number):
@@ -23,7 +53,16 @@ def negate(number):
     :param number: A bitstring representing the number to negate
     :return: A bistring representing the negated number
     """
-    pass
+
+    result = ''
+    for i in range(16):
+        if number[i] == '1':
+            result += '0'
+        else:
+            result += '1'
+    
+    one = '0000000000000001'
+    return add(result, one)
 
 
 def subtract(minuend, subtrahend):
@@ -35,7 +74,8 @@ def subtract(minuend, subtrahend):
     :param subtrahend: A bitstring representing the number to subtract
     :return: A bitstring representing the difference
     """
-    pass
+    negatron = negate(subtrahend)
+    return add(minuend, negatron)
 
 
 def multiply(multiplicand_a, multiplicand_b):
@@ -47,7 +87,28 @@ def multiply(multiplicand_a, multiplicand_b):
     :param multiplicand_b: A bitstring representing the second number
     :return: A bitstring representing the product
     """
-    pass
+    negative_count = 0
+    if (multiplicand_a[0] == '1'):
+        multiplicand_a = negate(multiplicand_a)
+        negative_count += 1
+    if (multiplicand_b[0] == '1'):
+        multiplicand_b = negate(multiplicand_b)
+        negative_count += 1
+    
+    result = '0000000000000000'
+    mults_from_one_digit = []
+    for i in range(16):
+        if (multiplicand_a[15-i] == '1'):
+            one_digit_product = multiplicand_b[i:] + '0'*i
+            mults_from_one_digit.append(one_digit_product)
+
+    for i in range(len(mults_from_one_digit)):
+        result = add(result, mults_from_one_digit[i])
+    
+    if (negative_count == 1):
+        result = negate(result)
+    return result
+
 
 
 def binary_to_decimal(number):
@@ -58,7 +119,12 @@ def binary_to_decimal(number):
     :param number: A bitstring representing the number to convert
     :return: An integer, the converted number
     """
-    pass
+    total = 0
+    for i in range(len(number)-1, -1, -1):
+        if (number[i] == '1'):
+            total += 2 ** (15-i)
+    
+    return total
 
 
 def decimal_to_binary(number):
@@ -70,4 +136,12 @@ def decimal_to_binary(number):
     :return: A bitstring representing the converted number
     :raise OverflowError: If the number cannot be represented with 8 bits
     """
-    pass
+    if (number > 255 or number < -256):
+        raise OverflowError
+    
+    result = '0000000000000000'
+    for i in range(number):
+        result = add(result, '0000000000000001') # :)
+    
+    return result
+
--- hexadecimal.py
+++ hexadecimal.py
@@ -2,6 +2,24 @@
 # CSC 225, Assignment 1
 # Given code, Winter '20
 
+hex_translation_guide = [
+    ['0000', '0'],
+    ['0001', '1'],
+    ['0010', '2'],
+    ['0011', '3'],
+    ['0100', '4'],
+    ['0101', '5'],
+    ['0110', '6'],
+    ['0111', '7'],
+    ['1000', '8'],
+    ['1001', '9'],
+    ['1010', 'A'],
+    ['1011', 'B'],
+    ['1100', 'C'],
+    ['1101', 'D'],
+    ['1110', 'E'],
+    ['1111', 'F']
+]
 
 def binary_to_hex(number):
     """
@@ -11,7 +29,16 @@ def binary_to_hex(number):
     :param number: A bitstring representing the number to convert
     :return: A hexadecimal string, the converted number
     """
-    pass
+    hs = [number[:4], number[4:8], number[8:12], number[12:]]
+    result = '0x'
+
+    for h in hs:
+        for digit in hex_translation_guide:
+            if h == digit[0]:
+                result += digit[1]
+                break
+
+    return result
 
 
 def hex_to_binary(number):
@@ -22,4 +49,65 @@ def hex_to_binary(number):
     :param number: A hexadecimal string, the number to convert
     :return: A bitstring representing the converted number
     """
-    pass
+    real = number[2:]
+    result = ''
+
+    for char in real:
+        for digit in hex_translation_guide:
+            if char == digit[1]:
+                result += digit[0]
+                break
+    
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+for h in hs:
+        if h == '0000':
+            result += '0'
+        elif h == '0001':
+            result += '1'
+        elif h == '0010':
+            result += '2'
+        elif h == '0011':
+            result += '3'
+        elif h == '0100':
+            result += '4'
+        elif h == '0101':
+            result += '5'
+        elif h == '0110':
+            result += '6'
+        elif h == '0111':
+            result += '7'
+        elif h == '1000':
+            result += '8'
+        elif h == '1001':
+            result += '9'
+        elif h == '1010':
+            result += 'A'
+        elif h == '1011':
+            result += 'B'
+        elif h == '1100':
+            result += 'C'
+        elif h == '1101':
+            result += 'D'
+        elif h == '1110':
+            result += 'E'
+        else:
+            result += 'F'
+
+
+'''
